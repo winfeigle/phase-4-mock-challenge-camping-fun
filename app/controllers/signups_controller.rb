aaf6fd70d399e64bfc13e696cb2b273328a7930a
Win Feigle <win.feigle@gmail.com>
2023-01-05 07:34:26 -0700

    Add signup POST and error handling

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,18 @@
 class SignupsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        signup = Signup.create!(signup_params)
+        render json: signup.activity, status: :created
+    end
+
+    private
+
+    def signup_params
+        params.permit(:time, :camper_id, :activity_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :campers, only: [:index, :show, :create]
+  resources :signups, only: [:create]
   resources :activities, only: [:index, :destroy]
+  resources :campers, only: [:index, :show, :create]
 end
